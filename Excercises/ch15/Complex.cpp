7d9f77bf9b2fd8a21b546b9abab48b55c032d3b9
PB22-School <pjb77108@email.vccs.edu>
2023-12-05 09:13:18 -0500

    Operator Overloads

---
+++ Excercises/ch15/Complex.cpp
@@ -0,0 +1,91 @@
+#include "Complex.h"
+using namespace std;
+
+enum Flag {POLAR};
+
+void Complex::calculate_polar()
+{
+    mag = sqrt(real * real + imag * imag);
+    theta = atan(imag / real);
+    polar = true;
+}
+
+void Complex::calculate_cartesian()
+{
+    assert(polar == true);
+    real = mag * cos(theta);
+    imag = mag * sin(theta);
+    // I added a "polar = false"
+    polar = false
+}
+
+Complex::Complex(double m, double t) {
+    // cartesian
+    mag = m; theta = t;
+    polar = true;
+    calculate_cartesian();
+}
+
+Complex::Complex(double m, double t, Flag) {
+    // polar
+    mag = m; theta = t;
+    polar = true;
+}
+
+double Complex::get_real()
+{
+    return real;
+}
+
+double Complex::get_imag()
+{
+    return imag;
+}
+
+double Complex::get_mag()
+{
+    if (polar == false) calculate_polar();
+    return mag;
+}
+
+double Complex::get_theta()
+{
+    if (polar == false) calculate_polar();
+    return theta;
+}
+
+Complex Complex::operator + (const Complex& c)
+{
+    return Complex(real + c.real, imag + c.imag);
+}
+
+Complex Complex::operator * (Complex& c)
+{
+    if (polar == false) calculate_polar();
+    if (c.polar == false) c.calculate_polar();
+    return Complex(mag * c.mag, theta + c.theta, POLAR);
+}
+
+Complex Complex::operator - (const Complex& c) 
+{
+    return Complex(real - c.real, imag - c.imag);
+}
+
+Complex Complex::operator / (Complex& c) 
+{
+    if (polar == false) calculate_polar();
+    if (c.polar == false) c.calculate_polar();
+    return Complex(mag / c.mag, theta - c.theta, POLAR);
+}
+
+string Complex::str_cartesian()
+{
+    return to_string(get_real()) + " + " + to_string(get_imag()) + "i";
+}
+
+string Complex::str_polar()
+{
+    string theta = to_string(get_theta());
+    string mag = to_string(get_mag());
+    return mag  + "e^" + theta + "i";
+}
---
+++ Excercises/ch15/Complex.h
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <assert.h>
+using namespace std;
+
+enum Flag {POLAR};
+
+class Complex
+{
+    double real, imag;
+    double mag, theta;
+    bool polar;
+
+    // private accessors
+    void calculate_polar();
+    void calculate_cartesian();
+
+public:
+    // constructors
+    Complex();
+    Complex(double r, double i);
+    Complex(double m, double t, Flag);
+
+    // accessors
+    double get_real();
+    double get_imag();
+    double get_mag();
+    double get_theta();
+
+    // member functions
+    Complex operator + (const Complex& c);
+    Complex operator - (const Complex& c);
+    Complex operator * (Complex& c);
+    Complex operator / (Complex& c);
+    Complex abs();
+    string str_cartesian();
+    string str_polar();
+};
